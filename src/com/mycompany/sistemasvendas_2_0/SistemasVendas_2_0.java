fcb9e6d5377e718328054d846bf703ef823f9666
MILEN� GARALUZ KONDARZEWSKI <mileny.1948@aluno.pr.senac.br>
2023-09-14 17:23:31 -0300

    criação

---
+++ src/com/mycompany/sistemasvendas_2_0/SistemasVendas_2_0.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.sistemasvendas_2_0;
+
+/**
+ *
+ * @author mileny.1948
+ */
+public class SistemasVendas_2_0 {
+    public static void main(String[] args) {
+        
+    }
+    
+}
